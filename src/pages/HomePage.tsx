4805c5c3f7ff678b358dc61a09dc9f338256f89f
Ahmed_Abdullah <ahmed.abdullah.nassar@gmail.com>
2025-04-20 07:04:57 -0800

    build for redeploing

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -110,7 +110,7 @@ const HomePage: React.FC = () => {
             {[1, 2, 3, 4, 5, 6].map(i => (
               <div key={i} className="animate-pulse bg-white rounded-lg overflow-hidden shadow-md h-48">
                 <div className="h-24 bg-gray-300"></div>
-                <div className="p-4 space-y-2">
+                <div className="p-4 space-y-2" >
                   <div className="h-4 bg-gray-300 rounded w-3/4"></div>
                   <div className="h-3 bg-gray-300 rounded w-1/2"></div>
                   <div className="h-3 bg-gray-300 rounded w-1/4 mt-2"></div>
